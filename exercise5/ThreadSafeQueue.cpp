b827fc04df43c8f3abe64ed7a1e00d19e34c1f93
eneapane <eneapane1@gmail.com>
2025-07-02 16:07:03 +0200

    A thread-safe queue with locking and waiting

--- exercise5/ThreadSafeQueue.cpp
+++ exercise5/ThreadSafeQueue.cpp
@@ -6,34 +6,36 @@
 
 template<typename T>
 std::shared_ptr<T> ThreadSafeQueue<T>::try_pop() {
-    std::lock_guard<std::mutex> lock_guard(m);
-    if (data.empty()) {
-        return std::shared_ptr<T>();
-    }
-    std::shared_ptr<T> result = std::make_shared<T>(std::move(data.front()));
-    data.pop();
-    return result;
+    std::unique_ptr<node> old_head = try_pop_head();
+    return old_head ?
+            old_head->data :
+                std::shared_ptr<T>();
 }
 
 template<typename T>
 std::shared_ptr<T> ThreadSafeQueue<T>::wait_and_pop() {
-    std::unique_lock<std::mutex> lock(m);
-    cv.wait(lock, [&] -> bool { return !data.empty(); });
-    std::shared_ptr<T> result = std::make_shared<T>(std::move(data.front()));
-    data.pop();
-    return result;
+    std::unique_ptr<node> const old_head = wait_pop_head();
+    return old_head->data;
+
 }
 
 template<typename T>
 void ThreadSafeQueue<T>::push(T value) {
+    std::shared_ptr<T> new_data(std::make_shared<T>(std::move(value)));
+    std::unique_ptr<node> p(new node);
+    node* const new_tail = p.get();
     {
-        std::lock_guard lock_guard(m);
-        data.push(value);
+        std::lock_guard<std::mutex> tail_lock(tail_mutex);
+
+        tail->data = new_data;
+        tail->next = new_tail;
+        tail = new_tail; //invariant: tail points to an empty node
     }
-    cv.notify_one();
+    data_cond.notify_one();
 }
 
 template<typename T>
 bool ThreadSafeQueue<T>::empty() {
-    return data.empty();
+    std::lock_guard<std::mutex> head_lock(head_mutex);
+    return (head.get() == get_tail());
 }
--- exercise5/ThreadSafeQueue.h
+++ exercise5/ThreadSafeQueue.h
@@ -12,11 +12,48 @@
 template <typename T>
 class ThreadSafeQueue {
 private:
-    mutable std::mutex m;
-    std::queue<T> data;
-    std::condition_variable cv;
+    struct node {
+        std::shared_ptr<T> data;
+        std::unique_ptr<node> next;
+    };
+
+    std::mutex head_mutex;
+    std::unique_ptr<node> head;
+    std::mutex tail_mutex;
+    node* tail = nullptr;
+    std::condition_variable data_cond;
+
+    node* get_tail() {
+        std::lock_guard<std::mutex> lock(tail_mutex);
+        return tail;
+    }
+
+    std::shared_ptr<T> pop_head() {
+        std::unique_ptr<node> old_head = std::move(head);
+        head = std::move(old_head->next);
+        return old_head;
+    }
+
+    std::unique_lock<std::mutex> wait_for_data() {
+        std::unique_lock<std::mutex> head_lock(head_mutex);
+        data_cond.wait(head_lock, [&] {return head.get() != get_tail(); });
+        return std::move(head_lock);
+    }
+
+    std::unique_ptr<node> wait_pop_head() {
+        std::unique_lock<std::mutex> head_lock(wait_for_data());
+        return pop_head();
+    }
+
+    std::unique_ptr<node> try_pop_head() {
+        std::lock_guard<std::mutex> lock(head_mutex);
+        if (head.get() == get_tail()) {
+            return std::unique_ptr<node>(nullptr);
+        }
+        return pop_head();
+    }
 public:
-    ThreadSafeQueue() = default;
+    ThreadSafeQueue() : head(new node), tail(head.get()) {}; //initialize with dummy node
     ThreadSafeQueue(const ThreadSafeQueue&) = delete;
     [[no_discard]] std::shared_ptr<T> try_pop();
     [[no_discard]] std::shared_ptr<T> wait_and_pop();
